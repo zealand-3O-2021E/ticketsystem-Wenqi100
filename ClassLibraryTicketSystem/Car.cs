4db97036c82ac2ed6f559504a8e36d983dcc14fe
wenqi huang <wenqi100@gmail.com>
2021-09-26 15:16:32 +0200

    Task 2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    public class Car
+    {
+        /// <summary>
+        /// a class “Car”, which has the following properties:
+        /// public string Licenseplate
+        /// public DateTime Date
+        /// </summary>
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// public constructor Car 
+        /// </summary>
+        public Car()
+        {
+
+        }
+
+        /// <summary>
+        /// method 
+        /// public double Price() 
+        /// </summary>
+        /// <returns>double Price() = 240 as fixed price</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// method
+        /// public string VehicleType() 
+        /// </summary>
+        /// <returns>"Car"</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using ClassLibraryTicketSystem;
+using System.Runtime.ConstrainedExecution;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        // Arrange
+        //Mock mock = new Mock<Car>();       
+
+        /// <summary>
+        /// Test Method: WhenCallPriceMethod_Return240
+        /// </summary>
+
+        [TestMethod]
+        public void WhenCallPriceMethod_Return240()
+        {
+            // Arrange
+            Car car = new Car();
+
+            //var mock = new Mock<Car>();
+            //var car = mock.Object;
+
+            // Act and Assert
+            var actual = car.Price();
+            Assert.AreEqual(240, actual);
+        }
+
+
+        /// <summary>
+        /// Test Method: WhenCallVehicleMethod_ReturnStringCar
+        /// </summary>
+        [TestMethod]
+        public void WhenCallVehicleMethod_ReturnStringCar()
+        {
+            // Arrange
+            Car car = new Car();
+
+            // Act and Assert
+            var actual = car.VehicleType();
+            Assert.AreEqual("Car", actual);
+        }
+    }
+}
