66de3524e2a7193ffd9ea35e70fd09e33d40d074
wenqi huang <wenqi100@gmail.com>
2021-09-26 18:42:26 +0200

    Test of VehicleType() method

--- ClassLibraryTicketSystemTests/CombinedTests.cs
+++ ClassLibraryTicketSystemTests/CombinedTests.cs
@@ -15,7 +15,7 @@ namespace ClassLibraryTicketSystemTests
         // Test of Price() method
         [DataTestMethod]
         [DynamicData(nameof(When_Call_PriceMethod_ReturnFixedValue), DynamicDataSourceType.Method)]
-        public void PriceMethod_ForEachVehicle_Test(VehicleBase vehicleBase, double expectedValue)
+        public void PriceMethod_ForEachVehicle(VehicleBase vehicleBase, double expectedValue)
         {
             var actual = vehicleBase.Price();
             Assert.AreEqual(expectedValue, actual);
@@ -27,10 +27,25 @@ namespace ClassLibraryTicketSystemTests
             yield return new object[] { new MC(), 125 };
         }
 
+        // Test of VehicleType() method
+        [DataTestMethod]
+        [DynamicData(nameof(When_Call_VehicleTypeMethod_ReturnVehicleType), DynamicDataSourceType.Method)]
+        public void VehicleTypeMethod_ForEachVehicle(VehicleBase vehicleBase, string expectedValue)
+        {
+            var actual = vehicleBase.VehicleType();
+            Assert.AreEqual(expectedValue, actual);
+        }
+
+        public static IEnumerable<object[]> When_Call_VehicleTypeMethod_ReturnVehicleType()
+        {
+            yield return new object[] { new Car(), "Car" };
+            yield return new object[] { new MC(), "MC" };
+        }
+
         // Test of GetPriceByBrobizz(double discount) method
         [DataTestMethod]
         [DynamicData(nameof(When_Call_GetPrice_ByBrobizz_Return_Price), DynamicDataSourceType.Method)]
-        public void PriceMethod_ByBrobizz_ForEachVehicle_Test(VehicleBase vehicleBase, double expectedValue)
+        public void PriceMethod_ByBrobizz_ForEachVehicle(VehicleBase vehicleBase, double expectedValue)
         {
             var actual = vehicleBase.GetPriceByBrobizz(0.05);
             Assert.AreEqual(expectedValue, actual);
@@ -42,25 +57,8 @@ namespace ClassLibraryTicketSystemTests
             yield return new object[] { new MC(false), 125 };
         }
 
-       
-
-
-        // Test of VehicleType() method
-        /*
-        [DataTestMethod]
-        [DynamicData(nameof(VehicleTypeMethodTestData), DynamicDataSourceType.Method)]
-        public void VehicleTypeMethodTestData(VehicleBase vehicleBase, string expectedValue)
-        {
-            var actual = vehicleBase.VehicleType();
-            Assert.AreEqual(expectedValue, actual);
-        }
-
-        public static IEnumerable<object[]> VehicleTypeMethodTestData()
-        {
-            yield return new object[] { new Car(), "Car" };
-            yield return new object[] { new MC(), "MC" };
-        }
-        */
+        
+        
 
         // Test of IsLengthCorrect(string licenseplate) method
         /*
